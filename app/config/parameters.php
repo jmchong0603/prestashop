2ac6a40ef856133ef64621977ccf49f75823673b
KienHaw <kienhaw2000@hotmail.com>
2023-08-29 15:53:16 +0800

    add file

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop_db',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'mAhj4xyKpUIr90BbqwAnL92KYsFwtXuFaovk0KPMdZCVBtsjNreFWkRF8v32pTmF',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-08-29',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'nXGh4rTN1haOsQfhLxxef1xV42E8knLc38hq9KSti1CCbKW6x7VbwZeVJe9Pa2g6',
+    'cookie_iv' => 'DgiAakB06KDPc8EZVrbDIu00Jxx0owqp',
+    'new_cookie_key' => 'def000001812ec87a5c7ba59095363e91bddedd46d386d2ce405fa01a1931db1038117d9710bb641ab979a19694dea31121767517445b7273e0785549dfed58a7b865a88',
+  ),
+);
